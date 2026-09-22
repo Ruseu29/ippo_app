ec7f4c9043fc74ed005115b68ce4fbee83a20df7
Ruseu29 <170157820+Ruseu29@users.noreply.github.com>
2026-09-22 14:26:37 +0900

    ci: GitHub Pagesへの自動公開を追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // GitHub Pagesのサブディレクトリでも、生成したファイルを読み込めるようにする。
+  base: "./",
   plugins: [react()],
 });
